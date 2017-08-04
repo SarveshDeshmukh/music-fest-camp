1f09205c646c2ef25878d7a4c1743f12068a63c0
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-08-04 11:30:44 +0000

    duplicate the images to test the spacing

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.get("/", function(req, res){
 var festivals = [
             {name: "UMF", image: "https://farm4.staticflickr.com/3939/32084181014_0011343533.jpg"},
             {name: "Tomorrowland", image: "https://farm1.staticflickr.com/523/32581377422_aa3065b707.jpg"},
+            {name: "Rock in Rio", image: "https://farm8.staticflickr.com/7111/7134797481_442e2a70a1.jpg"},
+            {name: "UMF", image: "https://farm4.staticflickr.com/3939/32084181014_0011343533.jpg"},
+            {name: "Tomorrowland", image: "https://farm1.staticflickr.com/523/32581377422_aa3065b707.jpg"},
+            {name: "Rock in Rio", image: "https://farm8.staticflickr.com/7111/7134797481_442e2a70a1.jpg"},
+             {name: "UMF", image: "https://farm4.staticflickr.com/3939/32084181014_0011343533.jpg"},
+            {name: "Tomorrowland", image: "https://farm1.staticflickr.com/523/32581377422_aa3065b707.jpg"},
             {name: "Rock in Rio", image: "https://farm8.staticflickr.com/7111/7134797481_442e2a70a1.jpg"}
         ];
         
