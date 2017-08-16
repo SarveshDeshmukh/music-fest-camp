25809d09d7e936c06f640b6567d5d358e3d699cb
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-08-16 12:58:37 +0000

    add route to write a new comment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express      = require("express"),
     bodyParser   = require("body-parser"), 
     mongoose     = require("mongoose"),
     Festival     = require("./models/festival"),
+    Comment = require("./models/comment");
     seedDB       = require("./seeds");
     
 seedDB();
@@ -24,7 +25,7 @@ app.get("/festivals", function(req, res){
         if(err){
             
         }else{
-            res.render("index", {festivals: allFestivals});
+            res.render("festivals/index", {festivals: allFestivals});
         }
     });
     
@@ -53,8 +54,10 @@ app.post("/festivals", function(req, res){
 
     //show form to create new festival
 app.get("/festivals/new", function(req, res){
-    res.render("new.ejs");
+    res.render("festivals/new");
 });
+
+
 
     //SHOW - shows more info about one festival
 app.get("/festivals/:id", function(req, res){
@@ -64,11 +67,49 @@ app.get("/festivals/:id", function(req, res){
             console.log(err);
         }else{
             //render show template with that festival
-            res.render("show",{festival : foundFestival});
+            res.render("festivals/show",{festival : foundFestival});
         }
     });
     
 });
+
+//=================
+//COMMENT ROUTES
+//================
+
+app.get("/festivals/:id/comments/new", function(req, res){
+    Festival.findById(req.params.id, function(err, festival){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("comments/new",{festival: festival});
+        }
+    });
+    
+});
+
+app.post("/festivals/:id/comments", function(req, res){
+    //Lookup festival using ID
+    Festival.findById(req.params.id, function(err, festival){
+        if(err){
+            console.log(err);
+        }else{
+    //Create a comment
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    festival.comments.push(comment);
+                    festival.save();
+                    res.redirect("/festivals/"+festival._id);
+                }
+           });
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Music Festival Camp has started!!!");
