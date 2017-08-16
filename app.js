ae63930200ca9ddfdda2b7f8711504d6823b8655
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-08-16 08:29:16 +0000

    add call to seedDB

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 var express      = require("express"), 
     app          = express(), 
     bodyParser   = require("body-parser"), 
-    mongoose     = require("mongoose");
-
+    mongoose     = require("mongoose"),
+    Festival     = require("./models/festival"),
+    seedDB       = require("./seeds");
+    
+seedDB();
 mongoose.connect("mongodb://localhost/music_fest_camp", {useMongoClient : true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Schema Setup
-
-var festivalSchema = new mongoose.Schema({
-    name : String,
-    image : String
-});
-
-var Festival = mongoose.model("Festival", festivalSchema);
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -28,7 +24,7 @@ app.get("/festivals", function(req, res){
         if(err){
             
         }else{
-            res.render("festivals", {festivals: allFestivals});
+            res.render("index", {festivals: allFestivals});
         }
     });
     
@@ -40,7 +36,8 @@ app.post("/festivals", function(req, res){
     //Get the form data
     var festName = req.body.festName;
     var festImg = req.body.festImg;
-    var newFest = {name :festName, image : festImg};
+    var description = req.body.festDescription;
+    var newFest = {name :festName, image : festImg, description : description};
     
     //Create a new festival and save to database;
     
@@ -54,11 +51,24 @@ app.post("/festivals", function(req, res){
     });
 });
 
-    //SHOW form to create new festival
+    //show form to create new festival
 app.get("/festivals/new", function(req, res){
     res.render("new.ejs");
 });
 
+    //SHOW - shows more info about one festival
+app.get("/festivals/:id", function(req, res){
+    //Find the festival with provided ID
+    Festival.findById(req.params.id).populate("comments").exec(function(err, foundFestival){
+        if(err){
+            console.log(err);
+        }else{
+            //render show template with that festival
+            res.render("show",{festival : foundFestival});
+        }
+    });
+    
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Music Festival Camp has started!!!");
