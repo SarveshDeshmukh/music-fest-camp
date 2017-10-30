64d4450092f3741e6cb0a9ccfa42dd5a0822c1b9
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-10-30 03:39:03 +0000

    add delete comment route and change id delete-form to class delete-form

--- routes/comments.js
+++ routes/comments.js
@@ -74,6 +74,18 @@ router.put("/:comment_id",function(req, res){
    });
 });
 
+//COMMENT DESTROY
+
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       } else{
+           res.redirect("/festivals/"+req.params.id);
+       }
+    });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
